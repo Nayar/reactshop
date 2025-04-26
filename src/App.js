c4773a291f7e896e4eabd33ddae0cfa27af596df
Nayar Joolfoo <nayar@joolfoo.com>
2025-04-26 10:47:51 +0400

    3 components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import CarShop from './CarShop';
+import Home from './Home';
+import About from './About';
+import MyProjects from './Projects';
 
 function App() {
   return (
-    <CarShop></CarShop>
+    <>
+      <Home></Home>
+      <About></About>
+      <MyProjects/>
+      {/* <CarShop></CarShop> */}
+    </>
   );
 }
 
