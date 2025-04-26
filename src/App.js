2c6e89be9fa1924763613a076dc525c0a2367258
Nayar Joolfoo <nayar@joolfoo.com>
2025-04-26 10:56:23 +0400

    made nav

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import CarShop from './CarShop';
 import Home from './Home';
 import About from './About';
 import MyProjects from './Projects';
+import { Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
+        </ul>
+      </nav>
       <Home></Home>
       <About></About>
       <MyProjects/>
