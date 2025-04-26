861085bc86ebc7571a4209972ac178673b6bf0c8
Nayar Joolfoo <nayar@joolfoo.com>
2025-04-26 09:30:47 +0400

    makde carshop component

---
+++ src/CarShop.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class CarShop extends React.Component {
+    render() {
+        return <>
+            <h1>Car Shop</h1>
+        </>
+    }
+}
+
+export default CarShop
